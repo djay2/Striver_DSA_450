8f71a1a6be24ee03d37a7f19f6b73cd2249cd110
Dhananjay Pundir <dhananjay.pundir02@gmail.com>
2025-10-08 18:32:45 +0530

    Easy folder

---
+++ Arrays/easy/ArrayIsPalindrome.java
@@ -0,0 +1,32 @@
+package Easy;
+//        | Metric           | Complexity |
+//        | ---------------- | ---------- |
+//        | Time Complexity  | `O(n)`     |
+//        | Space Complexity | `O(1)`     |
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayIsPalindrome {
+    public static void main(String[] args) {
+
+        List<Integer> integers = Arrays.asList(12, 23, 87, -1,-1,9, 87, 23, 12);
+
+        int left=0;
+        int right=integers.size()-1;
+        boolean isPalindrome=false;
+        while(left<right)
+        {
+            if(integers.get(left)==integers.get(right))
+                isPalindrome=true;
+            else
+                isPalindrome=false;
+            left++;
+            right--;
+        }
+        if(isPalindrome)
+            System.out.println("Array is Palindrome");
+        else
+            System.out.println("Array is  not Palindrome");
+    }
+}
