a6ffa5ea7d731acf41f2c88f0e751afd12d2280a
Dhananjay Pundir <dhananjay.pundir02@gmail.com>
2025-10-12 07:51:29 +0530

    RotateByK

--- Arrays/Easy/RotateArrayByKPlace.java
+++ Arrays/Easy/RotateArrayByKPlace.java
@@ -13,19 +13,51 @@ public class RotateArrayByKPlace {
         List<Integer> integers = Arrays.asList(87, 92,101,120,12,0);
         List<Integer> integers1 = Arrays.asList(87, 92,101,120,12,23);
         List<Integer> integers2 = Arrays.asList(12, 23, 87, 92,101,120);
-        System.out.println(LeftRotateByKPlace(integers,3));
-        System.out.println(LeftRotateByKPlace(integers1,4));
-        System.out.println(LeftRotateByKPlace(integers2,5));
+        System.out.println(LeftRotateByKPlace(integers,3)); // left rotate
+      //  System.out.println(RightRotateByKPlace(integers,3));
+        System.out.println(RightRotateByKPlace(integers1,4)); // right rotate
+        System.out.println(LeftRotateByKPlace(integers2,5)); //left rotate
 
     }
+    // function for left rotation
    static List<Integer> LeftRotateByKPlace(List<Integer> arr,int place)
-    {   int size=arr.size();
-        int temp=arr.get(size-1);
-        for(int i=size-1;i>0;i--)
-        {
-            arr.set(i,arr.get(i-1));
+    { int size=arr.size();
+        // Normalize 'place' to ensure it's within array bounds
+        place%=size;
+
+        // Step 1: Reverse the entire array
+        reverse(arr,0,size-1);
+
+        // Step 2: Reverse the first part (which will become the rotated front)
+        reverse(arr,0,size-place-1);
+
+        // Step 3: Reverse the second part (which will become the rotated back)
+        reverse(arr,size-place,size-1);
+
+
+        return arr;
+    }
+
+    // function for right rotation
+    static List<Integer> RightRotateByKPlace(List<Integer> arr,int place)
+    {
+       int size2=arr.size();
+
+        place%=size2; // Normalize 'place' to ensure it's within array bounds
+
+        // use left rotation function by passing size2-place
+        return LeftRotateByKPlace(arr,size2-place);
+    }
+
+    static List<Integer> reverse(List<Integer> arr, int start,int end)
+    {
+        while(start<end)
+        { int temp=arr.get(start);
+            arr.set(start,arr.get(end));
+            arr.set(end,temp);
+            start++; end--;
         }
-        arr.set(0,temp);
+
         return arr;
     }
 }
